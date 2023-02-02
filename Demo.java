6eab377abb4a64b2f8eda88183f05148f185a8f5
Nachiket <nachikettawar1099@gmainl.com>
2023-02-02 15:38:34 +0530

    Second commit

--- Demo.java
+++ Demo.java
@@ -7,7 +7,22 @@ this.data=data;
 this.node=node;
 }
 }
+
+class LinkedList{
+Node head;
+
+public void insert(int data){
+Node newNode = new Node(data);
+newNode.next =head;
+head=newNode;
+}
+public void Display(){
+
+}
+}
+
 public static void main(String args[]){
 
-
+LinkedList list = new LinedList();
+list.insert(1);
 }
